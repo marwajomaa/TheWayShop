52ab6b912a17fa1669a47dfca8e67aeef7ec574d
marwajomaa <marwajomaa4@gmail.com>
2020-12-23 21:31:37 +0200

    add cart style

--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,5 +1,13 @@
-import React, { useContext } from "react";
-import { Grid, Paper, Typography, makeStyles } from "@material-ui/core";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Grid,
+  Paper,
+  Typography,
+  IconButton,
+  makeStyles,
+} from "@material-ui/core";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 import { GlobalState } from "../../GlobalState";
 import img from "../../assets/img.jpg";
 import Button from "../../components/Button";
@@ -7,13 +15,12 @@ import Button from "../../components/Button";
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    flexWrap: "wrap",
+    maxWidth: "650px",
     padding: "30px 10px",
-    margin: "20px auto",
-    fontSize: "15%",
+    margin: "0 auto",
+    position: "relative",
+    boxShadow: "0 0 10px #eee",
+    transform: "scaleY(0.98)",
   },
   boxDetails: {
     maxWidth: "400px",
@@ -26,10 +33,32 @@ const useStyles = makeStyles((theme) => ({
     textTransform: "uppercase",
     fontWeight: "bold",
   },
-  Typography: {
+  typography: {
     lineHeight: "1.5",
     margin: "15px 0",
     opacity: ".8",
+    color: "darkblue",
+  },
+  deleteIcon: {
+    position: "absolute",
+    top: "0",
+    right: "0",
+    color: "crimson",
+    padding: "5px",
+  },
+  button: {
+    width: "40px",
+    height: "40px",
+    border: "1px solid #eee",
+  },
+  total: {
+    width: "100%",
+    height: "50px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    maxWidth: "650px",
+    margin: "20px auto",
   },
 }));
 
@@ -38,6 +67,7 @@ function Cart() {
   const [isLoggedIn] = globalState.token;
   const [cart, setCart] = globalState.userAPI.cart;
   const [isAdmin] = globalState.userAPI.isAdmin;
+  const [total, setTotal] = useState(0);
   const classes = useStyles();
 
   if (cart.length === 0) {
@@ -49,53 +79,65 @@ function Cart() {
   }
 
   return (
-    <Paper elevation={0}>
-      {cart.map(({ title, price, description, quantity, content }) => {
-        return (
-          <Grid container xs={12} spacing={3} className={classes.root}>
-            <Grid item xs={12} sm={6}>
-              <img src={img} alt="img" style={{ width: "100%" }} />
-            </Grid>
-            <Grid item xs={12} sm={6} className={classes.boxDetails}>
-              <Typography variant="h3" component="h3" gutterBottom={2}>
-                {title}
-              </Typography>
-              <Typography variant="h4" component="p" gutterBottom={1}>
-                ${price * quantity}
-              </Typography>
-              <Typography variant="h4" component="p" gutterBottom={1}>
-                {description}
-              </Typography>
-              <Typography variant="h5" component="p" gutterBottom={2}>
-                {content}
-              </Typography>
-              <Grid item spacing={1} gutterBottom={2}>
-                <Button text="-" />
+    <>
+      <Paper elevation={0}>
+        {cart.map(({ title, price, description, quantity, content }) => {
+          return (
+            <Grid container xs={12} spacing={3} className={classes.root}>
+              <Grid item xs={12} sm={6}>
+                <img
+                  src={img}
+                  alt="img"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} className={classes.boxDetails}>
                 <Typography
                   variant="h4"
-                  component="span"
-                  style={{ textAlign: "center", padding: "20px 15px" }}
+                  component="h4"
+                  gutterBottom={2}
+                  className={classes.typography}
                 >
-                  {quantity}
+                  {title}
                 </Typography>
-                <Button text="+" style={{ margin: "20px" }} />
+                <Typography variant="h6" component="span" gutterBottom={1}>
+                  ${price * quantity}
+                </Typography>
+                <Typography variant="h5" component="p" gutterBottom={2}>
+                  {description}
+                </Typography>
+                <Typography variant="h6" component="p" gutterBottom={2}>
+                  {content}
+                </Typography>
+                <Grid item spacing={1} gutterBottom={2}>
+                  <Button text="-" className={classes.button} />
+                  <Typography
+                    variant="h4"
+                    component="span"
+                    style={{ textAlign: "center", padding: "20px 15px" }}
+                  >
+                    {quantity}
+                  </Typography>
+                  <Button text="+" className={classes.button} />
+                </Grid>
+                <IconButton
+                  onClick={() => console.log("helllo")}
+                  className={classes.deleteIcon}
+                >
+                  <HighlightOffIcon />
+                </IconButton>
               </Grid>
-              <Button
-                text="Delete"
-                color="secondary"
-                style={{ width: "120px", margin: "0 20px" }}
-              />
             </Grid>
-          </Grid>
-        );
-      })}
-      <Grid item>
-        <Typography variant="h3" component="p">
-          Total: $
+          );
+        })}
+      </Paper>
+      <Grid item className={classes.total}>
+        <Typography variant="h4" component="p">
+          Total: ${total}
         </Typography>
         <Button href="/cart" text="Payment" />
       </Grid>
-    </Paper>
+    </>
   );
 }
 
